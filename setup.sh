a8aca66cd2028ef17b738604e3edef7d92cfff9d
thrivethru <mrwarner@outlook.com>
2013-07-08 16:54:04 -0500

    Changed dotfiles repo from startup-class to thrivethru

--- setup.sh
+++ setup.sh
@@ -34,10 +34,9 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/thrivethru/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
-
